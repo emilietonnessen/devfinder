c0a3d539212e0a5d340278f48751e93e33aeadd7
Emilie Tønnessen <emilie@tonnessen.com>
2021-10-21 16:21:20 +0200

    Clean up expected behaviours if data is missing

--- src/components/User/UserScore.tsx
+++ src/components/User/UserScore.tsx
@@ -1,26 +1,26 @@
 interface IUserScoreProps {
-  repos: number;
-  followers: number;
-  following: number;
+	repos: number;
+	followers: number;
+	following: number;
 }
 
 const UserScore = ({ followers, following, repos }: IUserScoreProps) => {
-  return (
-    <div className='user-score'>
-      <p className='user-score__container'>
-        <span className='user-score__value'>Repos</span>
-        <span className='user-score__key'>{repos}</span>
-      </p>
-      <p className='user-score__container'>
-        <span className='user-score__value'>Followers</span>
-        <span className='user-score__key'>{followers}</span>
-      </p>
-      <p className='user-score__container'>
-        <span className='user-score__value'>Following</span>
-        <span className='user-score__key'>{following}</span>
-      </p>
-    </div>
-  );
+	return (
+		<div className='user-score'>
+			<p className='user-score__container'>
+				<span className='user-score__value'>Repos</span>
+				<span className='user-score__key'>{repos}</span>
+			</p>
+			<p className='user-score__container'>
+				<span className='user-score__value'>Followers</span>
+				<span className='user-score__key'>{followers}</span>
+			</p>
+			<p className='user-score__container'>
+				<span className='user-score__value'>Following</span>
+				<span className='user-score__key'>{following}</span>
+			</p>
+		</div>
+	);
 };
 
 export default UserScore;
--- src/store/reducers/search.ts
+++ src/store/reducers/search.ts
@@ -1,78 +1,78 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 interface IInitialState {
-  loading: boolean;
-  error: boolean;
-  user: IUser;
+	loading: boolean;
+	error: boolean;
+	user: IUser;
 }
 
 interface IUser {
-  avatar_url: string;
-  name: string;
-  login: string;
-  created_at: string;
-  bio: string;
-  public_repos: number;
-  followers: number;
-  following: number;
-  location: string;
-  blog: string;
-  twitter_username: string;
-  company: string;
+	avatar_url: string;
+	name: string;
+	login: string;
+	created_at: string;
+	bio: string;
+	public_repos: number;
+	followers: number;
+	following: number;
+	location: string;
+	blog: string;
+	twitter_username: string;
+	company: string;
 }
 
 const initialState: IInitialState = {
-  loading: false,
-  error: false,
-  user: {
-    avatar_url: '',
-    name: '',
-    login: '',
-    created_at: '',
-    bio: '',
-    public_repos: 0,
-    followers: 0,
-    following: 0,
-    location: '',
-    blog: '',
-    twitter_username: '',
-    company: '',
-  },
+	loading: false,
+	error: false,
+	user: {
+		avatar_url: '',
+		name: '',
+		login: '',
+		created_at: '',
+		bio: '',
+		public_repos: 0,
+		followers: 0,
+		following: 0,
+		location: '',
+		blog: '',
+		twitter_username: '',
+		company: '',
+	},
 };
 
 const searchSlice = createSlice({
-  name: 'search',
-  initialState,
-  reducers: {
-    fetchGitHubUserStart: (state) => {
-      return {
-        ...state,
-        loading: true,
-        error: false,
-      };
-    },
-    fetchGitHubUserSuccess: (state, action) => {
-      return {
-        ...state,
-        loading: false,
-        error: false,
-        user: action.payload,
-      };
-    },
-    fetchGitHubUserFailed: (state) => {
-      return {
-        ...state,
-        loading: false,
-        error: true,
-      };
-    },
-  },
+	name: 'search',
+	initialState,
+	reducers: {
+		fetchGitHubUserStart: (state) => {
+			return {
+				...state,
+				loading: true,
+				error: false,
+			};
+		},
+		fetchGitHubUserSuccess: (state, action) => {
+			return {
+				...state,
+				loading: false,
+				error: false,
+				user: action.payload,
+			};
+		},
+		fetchGitHubUserFailed: (state) => {
+			return {
+				...state,
+				loading: false,
+				error: true,
+			};
+		},
+	},
 });
 
 export const {
-  fetchGitHubUserStart,
-  fetchGitHubUserSuccess,
-  fetchGitHubUserFailed,
+	fetchGitHubUserStart,
+	fetchGitHubUserSuccess,
+	fetchGitHubUserFailed,
 } = searchSlice.actions;
 
 export default searchSlice.reducer;
